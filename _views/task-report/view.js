9fc1b08b6b560b540e114873348b85fa8dd720ef
Igor Timofeev <i.timofeev@global-system.ru>
2024-11-26 14:12:18 +0300

    feat: Добавить в отчет по времени задач колонку с отметками о выполнении задачи

--- _views/task-report/view.js
+++ _views/task-report/view.js
@@ -6,8 +6,11 @@ dv = input.dv;
 var ds = require(app.vault.adapter.basePath + "/_scripts/dataset.js");
 
 const folderName = dv.current().file.folder;
-const tasks = ds.getRawSprintTaskData(dv, `${folderName.slice(0,folderName.lastIndexOf("/"))}`)
+const sprintFolderName = folderName.slice(0,folderName.lastIndexOf("/"));
+const tasks = ds.getRawSprintTaskData(dv, sprintFolderName);
 console.log(tasks)
+
+const boardCards = dv.page(`${sprintFolderName}/board.md`).file.lists;
 
 // const taskPages = dv.pages(`"${folderName.slice(0,folderName.lastIndexOf("/"))}/tasks"`)
 const taskSumdata = []
@@ -17,19 +20,34 @@ tasks.forEach( rec => {
     console.log(spentSummary)
     let estimate = rec.page.file.frontmatter.estimate ? rec.page.file.frontmatter.estimate : 0
     estimate = parseFloat(Number(estimate).toFixed(1))
+
+    let isDone = false;
+    let boardColumnName = "";
+    for (const boardCard of boardCards) {
+        if (boardCard.text.includes(rec.page.file.name)) {
+            console.log(boardCard.header.subpath + " " + rec.page.file.name);
+            isDone = boardCard.header.subpath == "done";
+            boardColumnName = boardCard.header.subpath;
+            break;
+        }
+    }
+    const doneTag = isDone ? `<font color="green">${boardColumnName}</font>` : "";
+
     const taskData = []
     const spentSummaryTag = spentSummary > estimate ? `<font color="red">${spentSummary}</font>` : spentSummary;
-    taskData.push(`<b>${rec.page.file.link}</b>`, estimate, spentSummaryTag, rec.page.file.path.contains("predefined"), spentSummary);
+    console.log(rec.page.file)
+    taskData.push(`<b>${rec.page.file.link}</b>`, estimate, spentSummaryTag, rec.page.file.frontmatter.user, doneTag, rec.page.file.path.contains("predefined"), spentSummary);
+
     taskSumdata.push(taskData)
 })
 
 
 dv.header(2, "Сопутствующие задачи!")
 dv.table(["Задача", "Estimate", "Spent"], 
-		 dv.array(taskSumdata.filter(d => d[3])).sort(d => parseFloat(d[4]), "desc").map(d => d.slice(0,3))
-		 )
+    dv.array(taskSumdata.filter(d => d[5])).sort(d => parseFloat(d[6]), "desc").map(d => d.slice(0,3))
+)
 
 dv.header(2, "Задачи спринта")
-dv.table(["Задача", "Estimate", "Spent"], 
-		 dv.array(taskSumdata.filter(d => !d[3])).sort(d => parseFloat(d[4]), "desc").map(d => d.slice(0,3))
-		 )
+dv.table(["Задача", "Estimate", "Spent", "Исполнители", "State"], 
+    dv.array(taskSumdata.filter(d => !d[5])).sort(d => parseFloat(d[6]), "desc").map(d => d.slice(0,5))
+)
